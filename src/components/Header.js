c7468fe1a582955ba4ca3181bdaa9c3893700c77
PabloOch <pabloochoabalderrama251@gmail.com>
2022-10-15 13:37:57 -0300

    fix(link): correção no link para acesso a pagina home

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <main>
       <LinkPosition>
         <div>
-          <IconeJS href='https://my-portifolio-git-style-jonathanprojetos.vercel.app/projects' alt='Home'>JS</IconeJS>
+          <IconeJS href='https://my-portifolio-eta.vercel.app/' alt='Home'>JS</IconeJS>
         </div>
         <LinkHeader>
           <Link to="/about">Sobre</Link>
